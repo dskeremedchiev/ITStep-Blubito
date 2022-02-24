d93e169a973de038817631e5bcb0ef66709e7800
dskeremedchiev <dskeremedchiev@gmail.com>
2022-02-24 09:16:49 +0200

    Update ng-hack.js

--- angular/my-first-project/src/assets/ng-hack.js
+++ angular/my-first-project/src/assets/ng-hack.js
@@ -1,4 +1,4 @@
 // const els = document.querySelectorAll("app-partei h4");
-const els = document.querySelectorAll("body");
-console.log(els.innerHTML);
-console.log(document.body.innerHTML);
+// const els = document.querySelectorAll("body");
+// console.log(els.innerHTML);
+// console.log(document.body.innerHTML);
